50c1f4c074e94b8f8f3103c4b0790cab6f7de2f3
naokuro <balance22@gmail.com>
2020-01-23 12:19:40 +0900

    pumaの設定ファイルをデフォルトに戻す

--- config/puma.rb
+++ config/puma.rb
@@ -10,10 +10,6 @@ rackup DefaultRackup
 port ENV['PORT'] || 8080
 environment ENV['RACK_ENV'] || 'development'
 
-on_restart do
-  ENV['BUNDLE_GEMFILE'] = '/var/www/vhosts/karitoke.jp/errbit/current/Gemfile'
-end
-
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
